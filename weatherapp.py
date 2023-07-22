e3b02891b26f986b1b90d3dafe6e0edfb8221088
Prabal <aryalparbal@gmail.com>
2023-07-22 19:40:40 +0545

    any city works

--- weatherapp.py
+++ weatherapp.py
@@ -5,10 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 current_date_only = datetime.today().date().strftime("%d-%m-%Y")
 name=input("your city name= ").capitalize()
-response=requests.get('https://api.openweathermap.org/data/2.5/weather?q=Kathmandu&appid=c007f3fc3a422083d719d41a4a511272')
+response=requests.get('https://api.openweathermap.org/data/2.5/weather?q={}&appid=c007f3fc3a422083d719d41a4a511272'.format(name))
 weathertoday= response.json()
 weather_data= {
-    "Temperature": weathertoday["main"]["temp"]-273,
+    "Temperature": weathertoday["main"]["temp"]-273.15,
     "Humidity": weathertoday["main"]["humidity"],
     "Weather Description": weathertoday["weather"][0]["description"],
     "City": weathertoday["name"]
