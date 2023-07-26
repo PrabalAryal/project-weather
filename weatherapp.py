4a7072271ba2c11f3c703d947577247d3d0dc8de
Prabal <aryalparbal@gmail.com>
2023-07-26 21:40:41 +0545

    minor changes

--- weatherapp.py
+++ weatherapp.py
@@ -1,18 +1,23 @@
 import requests
 from datetime import datetime
 import pandas as pd
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
+
 current_date_only = datetime.today().date().strftime("%d-%m-%Y")
-name=input("your city name= ").capitalize()
-response=requests.get('https://api.openweathermap.org/data/2.5/weather?q={}&appid=c007f3fc3a422083d719d41a4a511272'.format(name))
-weathertoday= response.json()
-weather_data= {
-    "Temperature": weathertoday["main"]["temp"]-273.15,
+name = input("your city name= ").capitalize()
+response = requests.get(
+    "https://api.openweathermap.org/data/2.5/weather?q={}&appid=c007f3fc3a422083d719d41a4a511272".format(
+        name
+    )
+)
+weathertoday = response.json()
+weather_data = {
+    "Temperature": weathertoday["main"]["temp"] - 273.15,
     "Humidity": weathertoday["main"]["humidity"],
     "Weather Description": weathertoday["weather"][0]["description"],
-    "City": weathertoday["name"]
+    "City": weathertoday["name"],
 }
-weather=[weather_data]
-weather_kathmandu=pd.DataFrame(weather)
-print(weather_kathmandu.head())
+weather = [weather_data]
+weather_city = pd.DataFrame(weather)
+print(weather_city.head())
